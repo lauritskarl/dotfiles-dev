76db67d6468a4601b99d157539f499ed219134c9
Karl Hans Laurits <karl@laurits.dev>
2025-06-20 14:09:19 +0300

    Update config.fish

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
-    set --global fish_key_bindings fish_vi_key_bindings
     set --global fish_greeting
+    set --global fish_key_bindings fish_vi_key_bindings
     mise activate fish | source
     starship init fish | source
-    zoxide init --cmd cd fish | source
+    zoxide init fish --cmd cd | source
     set --global --export SHELL fish
     set --global --export EDITOR hx
     set --global --export PAGER bat
